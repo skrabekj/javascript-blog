05260cfd9a1d18a1d5a8bf8d7a2847630510bdc8
Jan Skrabek <jan.skrabek@tutanota.com>
2019-09-04 20:22:36 +0200

    updated json files

--- js/script.js
+++ js/script.js
@@ -29,12 +29,12 @@ function titleClickHandler(event){
   /* find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
 
-  console.log(targetArticle)
+  console.log(targetArticle);
   /* add class 'active' to the correct article */
   event.preventDefault();
   //const clickedElement = this;
   console.log('clickedArticle:', targetArticle);
-  targetArticle.classList.add("active");
+  targetArticle.classList.add('active');
 }
 
 const optArticleSelector = '.post',
@@ -54,18 +54,18 @@ function generateTitleLinks(){
   for(let article of articles){
 
     /* get the article id */
-      const articleId = article.getAttribute('id');
-      console.log(articleId);
+    const articleId = article.getAttribute('id');
+    console.log(articleId);
     /* find the title element */
-      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-      console.log(articleTitle)
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log(articleTitle)
     /* get the title from the title element */
-      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-      console.log(linkHTML)
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML)
     /* create HTML of the link */
-      titleList.insertAdjacentHTML('beforeEnd', linkHTML);
+    titleList.insertAdjacentHTML('beforeEnd', linkHTML);
     /* insert link into titleList */
-      html = html + linkHTML;
+    html = html + linkHTML;
   }
   titleList.innerHTML = html;
   console.log(html);
